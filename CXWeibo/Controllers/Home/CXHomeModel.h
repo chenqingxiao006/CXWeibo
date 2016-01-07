12387a81094af7de81e33b808afff5c31463a64e
chenqingxiao <chenqingxiao007@iCloud.com>
2016-01-07 14:46:08 +0800

    通过pod添加MJExtension

---
+++ CXWeibo/Controllers/Home/CXHomeModel.h
@@ -0,0 +1,15 @@
+//
+//  CXHomeModel.h
+//  CXWeibo
+//
+//  Created by 陈擎霄 on 16/1/7.
+//  Copyright © 2016年 chenqingxiao007. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CXHomeModel : NSObject
+
+- (instancetype)initWithDic:(NSDictionary *)dic;
+
+@end
---
+++ CXWeibo/Controllers/Home/CXHomeModel.m
@@ -0,0 +1,20 @@
+//
+//  CXHomeModel.m
+//  CXWeibo
+//
+//  Created by 陈擎霄 on 16/1/7.
+//  Copyright © 2016年 chenqingxiao007. All rights reserved.
+//
+
+#import "CXHomeModel.h"
+
+@implementation CXHomeModel
+
+- (instancetype)initWithDic:(NSDictionary *)dic{
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+@end
