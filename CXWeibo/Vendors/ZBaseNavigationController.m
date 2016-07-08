714c0e2f0162002ace33a918f2153ceed59443d2
crfchina-mac-mini-1000 <zqq0702@sina.com>
2016-07-08 10:30:52 +0800

    添加ZtabBarController及tabbar图片

---
+++ CXWeibo/Vendors/ZBaseNavigationController.m
@@ -0,0 +1,28 @@
+//
+//  ZBaseNavigationController.m
+//  ChatTest
+//
+//  Created by crfchina-mac-mini-1000 on 16/6/21.
+//  Copyright © 2016年 crfchina-mac-mini-1000. All rights reserved.
+//
+
+#import "ZBaseNavigationController.h"
+
+@interface ZBaseNavigationController ()
+
+@end
+
+@implementation ZBaseNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CXWeibo/Vendors/ZTabBarController/ZTabbarController.h
@@ -0,0 +1,13 @@
+//
+//  ZTabbarController.h
+//  ChatTest
+//
+//  Created by crfchina-mac-mini-1000 on 16/6/21.
+//  Copyright © 2016年 crfchina-mac-mini-1000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZTabbarController : UITabBarController
+
+@end
